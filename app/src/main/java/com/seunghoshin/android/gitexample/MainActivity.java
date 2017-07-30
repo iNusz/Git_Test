d2e8f46e5f23de4adf2162b34a0b3fe0c40aea31
SeungHo Shin <94inusz@gmail.com>
2017-07-30 14:27:34 +0900

    버튼을 추가했다.

--- app/src/main/java/com/seunghoshin/android/gitexample/MainActivity.java
+++ app/src/main/java/com/seunghoshin/android/gitexample/MainActivity.java
@@ -1,7 +1,8 @@
 package com.seunghoshin.android.gitexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button a = new Button(this);
     }
 }
